7f41cfb3f9bf46b19e75b7aed8595cbe0b0af7d0
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-07-07 11:49:22 +0300

    Update steph22.py

--- steph22.py
+++ steph22.py
@@ -6,7 +6,6 @@ import plotly.figure_factory as ff
 import matplotlib 
 import streamlit as st
 import pandas as pd
-import numpy as np
 from streamlit_option_menu import option_menu
 
 st.set_page_config(page_icon="images.jpg",layout="wide")
